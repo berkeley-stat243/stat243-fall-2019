8d2d363e398796ef466996c3cdd1eb8e0cede822
Christopher Paciorek <paciorek@stat.berkeley.edu>
2019-08-28 15:24:23 -0700

    added class1 log file

---
+++ units/unit1-class1-log.sh
@@ -0,0 +1,25 @@
+  500  pwd
+  501  ls /
+  502  pwd
+  503  ls
+  504  cd Desktop/
+  505  cd ../Downloads/
+  506  cd .
+  507  cd ../Desktop/
+  508  git clone https://github.com/berkeley-stat243/stat243-fall-2018
+  509  git clone https://github.com/berkeley-stat243/stat243-fall-2018
+  510  ls -lrt
+  511  ls -lrt
+  512  cd stat243-fall-2018
+  513  ls
+  514  cd ..
+  515  ls -l
+  516  man ls
+  517  man ls
+  518  ls --long
+  519  man cd
+  520  cd ..
+  521  ls
+  522  cd Desktop 
+  523  ssh paciorek@smeagol.berkeley.edu
+  526  history >> unit1-class1-log.sh
